ed239d7e3e253eba69e73256f7faa630e31ba12b
Janaka Dabare <ajkd@boc.lk>
2021-08-23 10:24:57 +0530

    Add files via upload

--- py/trfcntrl/trfcntrl.ino
+++ py/trfcntrl/trfcntrl.ino
@@ -49,8 +49,9 @@ void loop() {
       i = k + 1;  
     } while ( y.charAt(i) != '\n');
     if ( p[0].charAt(0) == '0' ) {
-      onoff(p,++j);
-      r="#ok\n";
+      r = "#ok " + onoff(p,++j) + "\n";
+
+//     r="#ok\n";
     } else {
         if ( p[0].charAt(0) == '1' ) {
            r = "#" + readsensor(p[1]) + "\n";
@@ -68,29 +69,52 @@ void loop() {
 }
 
     
-void onoff ( String p[], int x ) {
+String onoff ( String p[], int x ) {
   int l = 1;
+  String o = "";
   do {
     int i = 0;
     int j = 0;
-    String q[3];
+    String q[] = {"","","","",""};
+    int maxe=3;
     do {                            //   pin no, on(O) or off (F), deley (milli seconds ),
       int k = p[l].indexOf(',',i);
       q[j++] = p[l].substring(i,k);
+      if ( ( j == 2 ) &&  ( q[1].charAt(0) == 'B' ) )
+        maxe = 5;
       i = k + 1;  
-    } while ( j < 3 );
+    } while ( j < maxe );
+
     int dly = q[2].toInt();
     if ( dly > 0 )
       delay(dly);    
     int pinno = q[0].toInt();
-    pinMode(pinno, OUTPUT);  
-    if ( q[1].charAt(0)=='O')
-       digitalWrite(pinno, HIGH);  
-    else
-       digitalWrite(pinno, LOW);    
-    l++;   
+    char fc = q[1].charAt(0);
+    if ( fc == 'O' or fc == 'F') {
+      o = o +  q[0] + "  " + q[1] + "  " + q[2];
+      pinMode(pinno, OUTPUT);  
+      if ( fc =='O')
+        digitalWrite(pinno, HIGH);  
+      else
+        digitalWrite(pinno, LOW);
+    } else {
+        o = o +  q[0] + "  " + q[1] + "  " + q[2] + "  " + q[3] + "  " + q[4];
+        unsigned long time = millis()+ q[4].toInt();
+        int bf = q[3].toInt(); 
+        pinMode(pinno, OUTPUT); 
+        do {
+          digitalWrite(pinno, HIGH);
+          delay(bf);
+          digitalWrite(pinno, LOW);
+          delay(bf);
+        } while ( millis() < time );
+ 
+    }
+    l++;
+       
   } while ( l < x );
-     
+
+  return o;   
 }
 
 String readsensor( String p ) {
