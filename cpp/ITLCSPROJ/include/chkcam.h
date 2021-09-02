6fa8dd8b4962e5ebe4ebb5409d493b58e199b998
Janaka Dabare <ajkd@boc.lk>
2021-09-02 20:24:15 +0530

    Delete cpp/ITLCSPROJ directory

--- cpp/ITLCSPROJ/include/chkcam.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class chkcam {
-  public :
-	  chkcam();
-	  ~chkcam();
-	  int dimg(int camid);
-};
--- cpp/ITLCSPROJ/include/tlcmain.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class tlcmain {
-  public:
-	tlcmain();
-	~tlcmain();
-	void prcs(char* sit);
-};
